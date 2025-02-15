51bdfbcfa31de6c525b5e96527013c31350e9356
Leejjon <leejjon@gmail.com>
2025-02-16 00:37:17 +0100

    Got the jOOQ codegen to work in gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.24"
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.jooq.jooq-codegen-gradle") version "3.19.18"
 }
 
 group = "net.leejjon"
@@ -21,16 +22,17 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("org.jetbrains.exposed:exposed-spring-boot-starter:0.58.0")
-	implementation("org.jetbrains.exposed:exposed-java-time:0.58.0")
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	// https://springdoc.org/#general-overview
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
+	implementation("org.jooq:jooq:3.19.18")
+	implementation("org.jooq:jooq-meta-extensions:3.19.18")
 
-//	runtimeOnly("org.hsqldb:hsqldb")
 	runtimeOnly("com.h2database:h2")
+	jooqCodegen("org.jooq:jooq-meta-extensions:3.19.18")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -47,3 +49,66 @@ kotlin {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+jooq {
+	configuration {
+		generator {
+			name = "org.jooq.codegen.JavaGenerator"
+			database {
+				name = "org.jooq.meta.extensions.ddl.DDLDatabase"
+				properties {
+
+					// Specify the location of your SQL script.
+					// You may use ant-style file matching, e.g. /path/**/to/*.sql
+					//
+					// Where:
+					// - ** matches any directory subtree
+					// - * matches any number of characters in a directory / file name
+					// - ? matches a single character in a directory / file name
+					property {
+						key = "scripts"
+						value = "src/main/resources/schema.sql"
+					}
+
+					// The sort order of the scripts within a directory, where:
+					//
+					// - semantic: sorts versions, e.g. v-3.10.0 is after v-3.9.0 (default)
+					// - alphanumeric: sorts strings, e.g. v-3.10.0 is before v-3.9.0
+					// - flyway: sorts files the same way as flyway does
+					// - none: doesn't sort directory contents after fetching them from the directory
+					property {
+						key = "sort"
+						value = "semantic"
+					}
+
+					// The default schema for unqualified objects:
+					//
+					// - public: all unqualified objects are located in the PUBLIC (upper case) schema
+					// - none: all unqualified objects are located in the default schema (default)
+					//
+					// This configuration can be overridden with the schema mapping feature
+					property {
+						key = "unqualifiedSchema"
+						value = "none"
+					}
+
+					// The default name case for unquoted objects:
+					//
+					// - as_is: unquoted object names are kept unquoted
+					// - upper: unquoted object names are turned into upper case (most databases)
+					// - lower: unquoted object names are turned into lower case (e.g. PostgreSQL)
+					property {
+						key = "defaultNameCase"
+						value = "as_is"
+					}
+				}
+			}
+			target {
+				packageName = "com.example.generated"
+				directory = "${layout.buildDirectory}/generated-src/jooq"
+			}
+		}
+	}
+}
+
+
--- src/main/kotlin/net/leejjon/crud/database/DbService.kt
+++ src/main/kotlin/net/leejjon/crud/database/DbService.kt
@@ -3,93 +3,32 @@ package net.leejjon.crud.database
 import io.github.oshai.kotlinlogging.KotlinLogging
 import net.leejjon.crud.model.NewPerson
 import net.leejjon.crud.model.Person
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.transactions.transaction
+
 import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
-import java.time.LocalDate
 
 @Service
 class DbService {
     private val logger = KotlinLogging.logger {}
-    fun getPersonsFromDb(): List<Person> = transaction {
-        PersonEntity.selectAll().map {
-                row -> row.toPerson()
-        }
+    fun getPersonsFromDb(): List<Person> = emptyList()
+
+    fun getPerson(id: Int): Person? = null
+
+    fun createPerson(person: NewPerson): Person? = null
+
+    fun deletePerson(id: Int) {
+        logger.error { "Could not find person with id $id" }
+        throw ResponseStatusException(HttpStatusCode.valueOf(404))
     }
 
-    fun getPerson(id: Int): Person? = transaction {
-        PersonEntity.selectAll().where { PersonEntity.id.eq(id) }.firstOrNull()?.toPerson()
+    fun updatePerson(person: Person): Person {
+        logger.error { "Could not find person with id ${person.id}" }
+        throw ResponseStatusException(HttpStatusCode.valueOf(404))
     }
 
-    fun createPerson(person: NewPerson): Person? = transaction {
-        PersonEntity.insert {
-            it[fullName] = person.fullName
-            it[dateOfBirth] = person.dateOfBirth
-            it[heightInMeters] = person.heightInMeters
-        }.resultedValues!!.firstOrNull()?.toPerson()
-    }
-
-    fun deletePerson(id: Int) = transaction {
-        val update = PersonEntity.deleteWhere { PersonEntity.id.eq(id) }
-        if (update != 1) {
-            logger.error { "Could not find person with id $id" }
-            throw ResponseStatusException(HttpStatusCode.valueOf(404))
-        }
-    }
-
-    fun updatePerson(person: Person): Person = transaction {
-        val update = PersonEntity.update({ PersonEntity.id.eq(person.id) }) {
-            it[fullName] = person.fullName
-            it[dateOfBirth] = person.dateOfBirth
-            it[heightInMeters] = person.heightInMeters
-        }
-
-        if (update != 1) {
-            logger.error { "Could not find person with id ${person.id}" }
-            throw ResponseStatusException(HttpStatusCode.valueOf(404))
-        } else {
-            val updatedPersonFromDb = getPerson(person.id)
-            if (updatedPersonFromDb != null) {
-                return@transaction updatedPersonFromDb
-            } else {
-                logger.error { "Could not update person with id $person.id" }
-                throw ResponseStatusException(HttpStatusCode.valueOf(500))
-            }
-        }
-    }
-
-    fun updatePersonAttributes(personId: Int, updatedFields: Map<String, Any?>): Person = transaction {
-        val filterColumnAndValue = updatedFields.entries.map {
-            return@map when (it.key) {
-                "fullName" -> Pair(PersonEntity.fullName, it.value as String)
-                "dateOfBirth" -> Pair(PersonEntity.dateOfBirth, LocalDate.parse(it.value as String))
-                "heightInMeters" -> Pair(PersonEntity.heightInMeters, it.value as Double)
-                else -> throw ResponseStatusException(HttpStatusCode.valueOf(400))
-            }
-        }
-
-        val update = PersonEntity.update({
-            PersonEntity.id.eq(personId)
-        }) {
-            filterColumnAndValue.forEach { (column, value) ->
-                it[column as Column<Any>] = value
-            }
-        }
-
-        if (update != 1) {
-            logger.error { "Could not find person with id ${personId}" }
-            throw ResponseStatusException(HttpStatusCode.valueOf(404))
-        } else {
-            val updatedPersonFromDb = getPerson(personId)
-            if (updatedPersonFromDb != null) {
-                return@transaction updatedPersonFromDb
-            } else {
-                logger.error { "Could not update person with id $personId" }
-                throw ResponseStatusException(HttpStatusCode.valueOf(500))
-            }
-        }
+    fun updatePersonAttributes(personId: Int, updatedFields: Map<String, Any?>): Person {
+        logger.error { "Could not find person with id ${personId}" }
+        throw ResponseStatusException(HttpStatusCode.valueOf(404))
     }
 }
